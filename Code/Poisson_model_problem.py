7ed5e50e0d402527205de474cc0ddd44b7e597b5
Georg Eichler <georg_eichler@web.de>
2026-02-08 23:52:10 +0100

    Prototypic Model problem

---
+++ Code/Poisson_model_problem.py
@@ -0,0 +1,71 @@
+from skfem import *
+from skfem.models.poisson import laplace
+from skfem.helpers import grad 
+import numpy as np
+
+m = MeshTri.init_sqsymmetric().refined(2)
+e = ElementTriP1()
+
+#Define RHS
+def load_func(x, y):
+    return 1.0
+
+@LinearForm
+def load(v, w):
+    x, y = w.x
+    return load_func(x, y) * v
+
+# Residual estimator
+def eval_estimator(m, u):
+    # interior residual
+    basis = Basis(m, e)
+
+    @Functional
+    def interior_residual(w):
+        h = w.h
+        x, y = w.x
+        return h**2 * load_func(x, y)**2
+    
+    eta_K = interior_residual.elemental(basis, w = basis.interpolate(u))
+
+    fbasis = [InteriorFacetBasis(m, e, side=i) for i in [0,1]]
+    w = {'u' + str(i + 1): fbasis[i].interpolate(u) for i in [0, 1]}
+
+    @Functional
+    def edge_jump(w):
+        h = w.h
+        n = w.n
+        dw1 = grad(w['u1'])
+        dw2 = grad(w['u2'])
+        return h * ((dw1[0] - dw2[0]) * n[0] +
+                    (dw1[1] - dw2[1]) * n[1])**2
+
+    eta_E = edge_jump.elemental(fbasis[0], **w)
+
+    tmp = np.zeros(m.facets.shape[1])
+    np.add.at(tmp, fbasis[0].find, eta_E)
+    eta_E = np.sum(0.5 * tmp[m.t2f], axis=0)
+
+    return eta_K + eta_E
+
+if __name__ == "__main__":
+    from skfem.visuals.matplotlib import draw, plot
+
+    for itr in reversed(range(6)):
+        basis = Basis(m, e)
+
+        K = asm(laplace, basis)
+        f = asm(load, basis)
+
+        # Homogeneous Dirichlet u=0 on boundary:
+        # solve for interior nodes only
+        I = m.interior_nodes()
+        u = solve(*condense(K, f, I=I))
+
+        # refine
+        if itr > 0:
+            m = m.refined(adaptive_theta(eval_estimator(m, u))).smoothed()
+
+    # visualize final mesh + solution
+    ax = draw(m)
+    plot(m, u, ax=ax, shading='gouraud', colorbar=True).show()
