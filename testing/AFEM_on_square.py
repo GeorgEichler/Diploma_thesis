f4bf3ac15559744731118e75891f84b8989f5e00
Georg Eichler <georg_eichler@web.de>
2026-05-24 23:34:17 +0200

    Test run for the midpoint method

--- testing/AFEM_on_square.py
+++ testing/AFEM_on_square.py
@@ -1,7 +1,9 @@
+import numpy as np
 from pathlib import Path
 from afem.core.config import AFEMConfig
 from afem.core.afem import run_afem
 from afem.problems.rhs import manufactured_sine_2d
+from afem.utils.plotting import plot_solution_2d
 
 """
 Test of AFEM method for the Dirichlet problem
@@ -10,6 +12,10 @@ Test of AFEM method for the Dirichlet problem
 with exact solution u(x,y) = sin(pi*x)*sin(pi*y) and
 Dirichlet energy J(u) = pi^2/4
 """
+
+# exact solution
+def u(x: np.ndarray) -> np.ndarray:
+    return np.sin(np.pi*x[0])*np.sin(np.pi*x[1])
 
 if __name__ == "__main__":
     cfg = AFEMConfig(
@@ -24,4 +30,9 @@ if __name__ == "__main__":
         plot_every=3,
         compute_reference_error=True,
     )
-    run_afem(cfg, manufactured_sine_2d)
+    mesh,_,_ = run_afem(cfg, manufactured_sine_2d)
+    u_exact = u(mesh.p)
+    out = Path(cfg.output_dir)
+    plots = out / "plots"
+    plot_solution_2d(mesh, u_exact, plots / "exact_solution.png")
+    print(r"Energy of exact solution is J(u)= - pi^2/4 =" , -np.pi/4)
